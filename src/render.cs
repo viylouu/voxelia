851420aedd891fdfced6a2b1b99ff0814b7eb005
viylouu <honeefunnee@gmail.com>
2024-10-08 20:27:38 -0600

    removed unimportant stuff

--- src/render.cs
+++ src/render.cs
@@ -2,25 +2,8 @@
     static void rend(ICanvas c) {
         c.Clear(Color.Black);
 
-        if (!intro.introplayed)
-        { intro.dointro(c, dfont); return; }
-
-        /*illum.addlight(Mouse.Position, Window.Height/2, 1, ColorF.Red, 1, false);
-        illum.addlight(new Vector2(Window.Width,Window.Height)-Mouse.Position, Window.Height / 2, 1, ColorF.Green, 1, false);
-
-        illum.addobj(new Vector2(Window.Width/2, Window.Height/16), new Vector2(Window.Width/2+Window.Width/16,Window.Height/8+Window.Height/16), new Vector2(Window.Width/2-Window.Width/16,Window.Height/8+Window.Height/16));
-
-        illum.draw(c);
-
-        illum.clearscene();*/
-
-        grad.startpos = new Vector2(0, Window.Height);
-        grad.endpos = new Vector2(Window.Width, 0);
-        grad.col1 = ColorF.CornflowerBlue;
-        grad.col2 = ColorF.Chocolate;
-
-        c.Fill(grad);
-        c.DrawRect(0,0,Window.Width,Window.Height);
+        //if (!intro.introplayed)
+        //{ intro.dointro(c, dfont); return; }
 
         fontie.rendertext(c, dfont, $"{MathF.Round(1/Time.DeltaTime)} fps", 3,3, ColorF.White);
     }
